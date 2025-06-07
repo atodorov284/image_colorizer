541980827a08d701d273a27b6e94ea8fc516e237
atodorov284 <atodorov284@gmail.com>
2025-06-07 12:11:55 +0200

    small cleanup

--- src/models/vgg.py
+++ src/models/vgg.py
@@ -15,23 +15,23 @@ class VGGColorizationModel(BaseColorizationModel):
         self.ab_norm = 110.0
         norm_layer = nn.BatchNorm2d
 
-        model1 = [
+        self.model1 = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=True),
             nn.ReLU(True),
             norm_layer(64),
-        ]
+        )
 
-        model2 = [
+        self.model2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=True),
             nn.ReLU(True),
             norm_layer(128),
-        ]
+        )
 
-        model3 = [
+        self.model3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
@@ -39,9 +39,9 @@ class VGGColorizationModel(BaseColorizationModel):
             nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=True),
             nn.ReLU(True),
             norm_layer(256),
-        ]
+        )
 
-        model4 = [
+        self.model4 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
@@ -49,9 +49,9 @@ class VGGColorizationModel(BaseColorizationModel):
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             norm_layer(512),
-        ]
+        )
 
-        model5 = [
+        self.model5 = nn.Sequential(
             nn.Conv2d(
                 512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True
             ),
@@ -65,9 +65,9 @@ class VGGColorizationModel(BaseColorizationModel):
             ),
             nn.ReLU(True),
             norm_layer(512),
-        ]
+        )
 
-        model6 = [
+        self.model6 = nn.Sequential(
             nn.Conv2d(
                 512, 512, kernel_size=3, dilation=2, stride=1, padding=2, bias=True
             ),
@@ -81,9 +81,9 @@ class VGGColorizationModel(BaseColorizationModel):
             ),
             nn.ReLU(True),
             norm_layer(512),
-        ]
+        )
 
-        model7 = [
+        self.model7 = nn.Sequential(
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
@@ -91,9 +91,9 @@ class VGGColorizationModel(BaseColorizationModel):
             nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             norm_layer(512),
-        ]
+        )
 
-        model8 = [
+        self.model8 = nn.Sequential(
             nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
@@ -101,16 +101,7 @@ class VGGColorizationModel(BaseColorizationModel):
             nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=True),
             nn.ReLU(True),
             nn.Conv2d(256, 313, kernel_size=1, stride=1, padding=0, bias=True),
-        ]
-
-        self.model1 = nn.Sequential(*model1)
-        self.model2 = nn.Sequential(*model2)
-        self.model3 = nn.Sequential(*model3)
-        self.model4 = nn.Sequential(*model4)
-        self.model5 = nn.Sequential(*model5)
-        self.model6 = nn.Sequential(*model6)
-        self.model7 = nn.Sequential(*model7)
-        self.model8 = nn.Sequential(*model8)
+        )
 
         self.softmax = nn.Softmax(dim=1)
         self.model_out = nn.Conv2d(
